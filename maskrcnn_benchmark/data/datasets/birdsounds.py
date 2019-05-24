ea5f26d53d652c6360ac6ca31c51f32d0a0e5863
aperkes <perkes.ammon@gmail.com>
2019-05-24 18:32:46 -0400

    birdsound dataclass foundations
    Building up the new dataclass
    Lots still to do

--- maskrcnn_benchmark/data/datasets/birdsounds.py
+++ maskrcnn_benchmark/data/datasets/birdsounds.py
@@ -1,6 +1,6 @@
 # Custom dataset class for use in maskrcnn-benchmark
 # written by Ammon Perkes May, 2019
-# For questions email perkes.ammon@gmail.com
+# For questions, find me at github.com/aperkes
 
 import torch
 import torchvision
@@ -9,11 +9,19 @@ from maskrcnn_benchmark.structures.bounding_box import BoxList
 from maskrcnn_benchmark.structures.segmentation_mask import SegmentationMask
 
 
+## Birdsong dataset class
+# Once complete add this to 
+# maskrcnn_benchmark/data/datasets/__init__.py
+# maskrcnn_benchmark/config/paths_catalog.py
+
+# And for testing: 
+# maskrcnn_benchmark/data/datasets/evaluation/__init__.py
 class birdsoundDataset(object):
     def __init__(self, ann_file, spec_dir):
         # Check the data and compile some internal structure of data
          
     def __getitem__(self,idx):
+# load the image as a PIL image
         image = ...
 
         return image, boxlist, idx
