9b92c4e87e8caa97a651c528864217fdd8961687
aperkes <perkes.ammon@gmail.com>
2019-06-04 11:36:04 -0400

    Ready to add overlay code
    everything else is organized, need to add code for showing predictions
    but need to get the network working correctly first.

--- inference/bird/show_predictions.py
+++ inference/bird/show_predictions.py
@@ -1,14 +1,19 @@
 ## Code to visualize predictions
 
 import torch
+from maskrcnn_benchmark.data.datasets import bird
 
-## import bird? 
+## get files
+root_dir = '/home/ammon/Documents/Scripts/maskrcnn-benchmark'
+ann_file = root_dir + '/data/datasets/bird_annotation.csv'
+image_dir = root_dir + '/data/datasets/bird/images'
+prediction_file = root_dir + '/inference/bird/predictions0.pth'
 
 ## First load prediction: 
 all_predictions = torch.load(prediction_file)
 
 ## Load dataset
-bird_dataset = birds.BirdDataset(images, ann_file) 
+bird_dataset = birds.BirdDataset(ann_file,images_dir)
 
 ## Load image
 image = bird_dataset[idx]
