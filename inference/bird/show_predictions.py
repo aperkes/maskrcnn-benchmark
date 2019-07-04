7e1bc5e726aeef081f4c8e447be5b87e62cf4918
aperkes <perkes.ammon@gmail.com>
2019-07-04 16:51:30 -0400

    Show Predictions
    It works now. The network is severely over predicting however

--- inference/bird/show_predictions.py
+++ inference/bird/show_predictions.py
@@ -2,22 +2,55 @@
 
 import torch
 from maskrcnn_benchmark.data.datasets import bird
+from matplotlib import pyplot as plt
+from matplotlib import patches
+import numpy as np
 
 ## get files
 root_dir = '/home/ammon/Documents/Scripts/maskrcnn-benchmark'
-ann_file = root_dir + '/data/datasets/bird_annotation.csv'
-image_dir = root_dir + '/data/datasets/bird/images'
-prediction_file = root_dir + '/inference/bird/predictions0.pth'
+ann_file = root_dir + '/maskrcnn_benchmark/data/datasets/bird/bird_annotations.json'
+image_dir = root_dir + '/maskrcnn_benchmark/data/datasets/bird/images'
+prediction_file = root_dir + '/inference/bird/predictions.pth'
 
 ## First load prediction: 
 all_predictions = torch.load(prediction_file)
 
-## Load dataset
-bird_dataset = birds.BirdDataset(ann_file,images_dir)
+## Load dataset 
+bird_dataset = bird.BirdDataset(ann_file,image_dir)
 
-## Load image
-image = bird_dataset[idx]
-prediction = all_predictions[idx]
+## Load image, with its targets and predictions
+try:
+    i = sys.argv[1]
+except:
+    i = 0
 
-## Pull some code to overlay the predictions on the image
-#...
+img,target,idx = bird_dataset[i]
+target_boxes = target.bbox
+predictions = all_predictions[i]
+
+## I might need to do some checking here eventually...
+predicted_boxes = predictions.bbox
+
+fig,ax = plt.subplots()
+#img = np.log(img)
+ax.imshow(img,vmax = 10)
+
+
+#Add Predicted Rectangles
+for p in predicted_boxes:
+    x0,y0,x1,y1 = p
+    w = x1-x0
+    h = y1-y0
+    rect = patches.Rectangle((x0,y0),w,h,linewidth=1,edgecolor='r',facecolor='red',alpha=.20)
+    ax.add_patch(rect)
+
+#Add Target Rectangles
+for t in target_boxes:
+    x0,y0,x1,y1 = t
+    w = x1-x0
+    h = y1-y0
+    rect = patches.Rectangle((x0,y0),w,h,linewidth=1,edgecolor='w',facecolor='none')
+    ax.add_patch(rect)
+
+plt.show()
+#fig.savefig('test.png',dpi=150)
